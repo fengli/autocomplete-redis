c1a6cf47b486a38b580671cd49f1b83aceaf0ab0
Feng Li <feng.li@inria.fr>
2012-07-31 18:15:33 +0200

    Add python setup.py file.
        * autocomplete/__init__.py: Add version.
        * setup.py: New.

--- autocomplete/__init__.py
+++ autocomplete/__init__.py
@@ -1,5 +1,7 @@
 #-*- coding:utf-8 -*-
 
+VERSION = '0.1'
+
 import redis
 
 try:
---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+import autocomplete
+
+long_description = open('README.rst').read()
+
+setup(
+    name='autocomplete',
+    version=autocomplete.VERSION,
+    packages=['autocomplete', 
+              ],
+    description='Redis based autocompletion (build index and search query).',
+    long_description=long_description,
+    author='Feng Li',
+    author_email='okidogiii@gmail.com',
+    license='BSD License',
+    url='https://github.com/fengli/autocomplete-redis.git',
+    platforms=["any"],
+    classifiers=[
+        'Development Status :: 1 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
